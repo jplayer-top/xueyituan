f59d071b371a02540466df133037ebbe83a5b5bd
oblivion0001 <18366108542@163.com>
2018-10-30 10:59:07 +0800

    asdas

--- app/src/main/java/com/xueyituanchina/xueyituan/ui/adapter/AdapterItemClass.java
+++ app/src/main/java/com/xueyituanchina/xueyituan/ui/adapter/AdapterItemClass.java
@@ -24,7 +24,7 @@ class AdapterItemClass extends BaseQuickAdapter<HomeGoodsList.ListBean.Goodslist
     protected void convert(BaseViewHolder helper, HomeGoodsList.ListBean.GoodslistBean item) {
         helper.setText(R.id.tvGoodsName, item.goods_title)
                 .setText(R.id.tvGift, item.goods_subtitle)
-                .setText(R.id.tvGoodsPrice, String.format(Locale.CHINA, "￥%s", item.goodsBestPriceStr))
+                .setText(R.id.tvGoodsPrice, String.format(Locale.CHINA, "团购价：￥%s", item.goodsBestPriceStr))
                 .setText(R.id.tvLocalGoodsPrice, String.format(Locale.CHINA, "门市价：￥%s", item.goodsOrgPriceStr));
 
     }
